9681c8ce7b814cd9b72d2992913d780afb6bea15
Nurbek <cnurba@gmail.com>
2025-09-02 20:58:34 +0600

    fsdfd

--- lib/app/shop/stock/application/set_stock_provider/set_stock_state.dart
+++ lib/app/shop/stock/application/set_stock_provider/set_stock_state.dart
@@ -3,11 +3,11 @@
 
 import 'package:service_admin/app/shop/stock/domain/models/stock.model.dart';
 
-class SetStockState extends Equatable {
-  final String type;
-  final List<StockModel> items;
-  final StateType stateType;
-
-  @override
-  List<Object?> get props => throw UnimplementedError();
-}
+// class SetStockState extends Equatable {
+//   final String type;
+//   final List<StockModel> items;
+//   final StateType stateType;
+//
+//   @override
+//   List<Object?> get props => throw UnimplementedError();
+// }
